9e844442d7139a0fa7540c2ac43c723a3d467e3b
anangnov99@gmail.com <anangnov99@gmail.com>
2019-05-16 10:13:34 +0700

    footer fix

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,15 +1,21 @@
 <footer class="footer">
     <div class="container">
         <div class="row gap-y align-items-center">
-            <div class="col-6 col-lg-6">
+            <div class="col-6 col-lg-3">
                 <a href="../index.html"><img src="{{ asset('web/assets/img/logo-dark.png') }}" alt="logo"></a>
             </div>
-
-            <div class="col-6 col-lg-6 text-right order-lg-last">
+            
+            <div class="col-6 col-lg-3 text-right order-lg-last">
                 <div class="social">
                     <a class="social-facebook" href="https://www.facebook.com/thethemeio"><i class="fa fa-facebook"></i></a>
                     <a class="social-instagram" href="https://www.instagram.com/thethemeio/"><i class="fa fa-instagram"></i></a>
                 </div>
+            </div>
+            
+            <div class="col-lg-6">
+                <div class="nav nav-bold nav-uppercase nav-trim justify-content-lg-center">
+                    <a>made with &hearts;</a>
+                </div>    
             </div>
         </div>
     </div>
