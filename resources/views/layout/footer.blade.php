434ab2c6a9f56d8ff32c6aac0cb9b21b21d66e3c
anangnov99@gmail.com <anangnov99@gmail.com>
2019-06-09 23:35:19 +0700

    css

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="footer">
+{{-- <footer class="footer">
     <div class="container">
         <div class="row gap-y align-items-center">
             <div class="col-6 col-lg-3">
@@ -17,6 +17,31 @@
                     <a>made with &hearts;</a>
                 </div>    
             </div>
+        </div>
+    </div>
+</footer> --}}
+
+<footer class="footer">
+    <div class="container">
+        <div class="row gap-y align-items-center">
+
+        <div class="col-6 col-lg-3">
+            <a href="../index.html"><img src="{{ asset('web/assets/img/logo-nang-dark.png') }}" alt="logo"></a>
+        </div>
+
+        <div class="col-6 col-lg-3 text-right order-lg-last">
+            <div class="social">
+                <a class="social-facebook" href="https://www.facebook.com/thethemeio"><i class="fa fa-facebook"></i></a>
+                <a class="social-instagram" href="https://www.instagram.com/thethemeio/"><i class="fa fa-instagram"></i></a>
+            </div>
+        </div>
+
+        <div class="col-lg-6">
+            <div class="nav nav-bold nav-uppercase nav-trim justify-content-lg-center">
+                <a class="nav-link" href="../uikit/index.html">made with &hearts;</a>
+            </div>
+        </div>
+
         </div>
     </div>
 </footer>
